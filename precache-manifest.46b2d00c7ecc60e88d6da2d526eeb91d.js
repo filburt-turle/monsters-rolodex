8f102f0bc23988db56d4105dce166f4a9b0d09f1
Max Orelus <max@maxorelus.com>
2020-05-28 16:22:32 -0400

    Updates

---
+++ precache-manifest.46b2d00c7ecc60e88d6da2d526eeb91d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52b4aba74d2725300bb0e3530f62d51",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f4cd5f5b8c9ebf4b8d0e",
+    "url": "/monsters-rolodex/static/css/main.24cc29ad.chunk.css"
+  },
+  {
+    "revision": "d687c1f9d751eecf4fb8",
+    "url": "/monsters-rolodex/static/js/2.1dd7b907.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1dd7b907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4cd5f5b8c9ebf4b8d0e",
+    "url": "/monsters-rolodex/static/js/main.14191e8f.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
